840eb66b8fb5fcc56ec4e314c6b9460d06ac000b
munjal <mupatel@hospnet.ca>
2019-06-28 14:48:36 -0400

    Add snapshot tests for React components

--- react-client/src/Form.test.js
+++ react-client/src/Form.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Form from './Form';
+import Form from './pages/Form';
+import renderer from 'react-test-renderer';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Form />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('matches the snapshot', () => {
+  const tree = renderer.create(<Form />).toJSON();
+  expect(tree).toMatchSnapshot();
 });
